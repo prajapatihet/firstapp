dc04a89016570d7d3cfd828f6518758b1ace9291
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-09 19:22:03 +0530

    current time

--- lib/src/present/views/main_screen/main_home.dart
+++ lib/src/present/views/main_screen/main_home.dart
@@ -9,6 +9,7 @@ class MainHomeScreen extends StatefulWidget {
 }
 
 class _MainHomeScreenState extends State<MainHomeScreen> {
+  var time = DateTime.now();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,125 +18,26 @@ class _MainHomeScreenState extends State<MainHomeScreen> {
         title: const Text('Flutter Demo'),
         elevation: 2,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              Text(
-                'ScrollView',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Current Time -> ${time.hour}:${time.minute}:${time.second}',
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
               ),
-              SizedBox(
-                height: 20,
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(right: 10),
-                      width: 200,
-                      height: 200,
-                      color: Colors.blue,
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(right: 10),
-                      width: 200,
-                      height: 200,
-                      color: Colors.orange,
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(right: 10),
-                      width: 200,
-                      height: 200,
-                      color: Colors.red,
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(right: 10),
-                      width: 200,
-                      height: 200,
-                      color: Colors.green,
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(right: 10),
-                      width: 200,
-                      height: 200,
-                      color: Colors.grey,
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(right: 10),
-                      width: 200,
-                      height: 200,
-                      color: Colors.blue,
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(right: 10),
-                      width: 200,
-                      height: 200,
-                      color: Colors.orange,
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                margin: EdgeInsets.only(bottom: 10),
-                height: 200,
-                color: Colors.redAccent,
-              ),
-              Container(
-                margin: EdgeInsets.only(bottom: 10),
-                height: 200,
-                color: Colors.orange,
-              ),
-              Container(
-                margin: EdgeInsets.only(bottom: 10),
-                height: 200,
-                color: Colors.red,
-              ),
-              Container(
-                margin: EdgeInsets.only(bottom: 10),
-                height: 200,
-                color: Colors.green,
-              ),
-              Container(
-                margin: EdgeInsets.only(bottom: 10),
-                height: 200,
-                color: Colors.grey,
-              ),
-              Container(
-                margin: EdgeInsets.only(bottom: 10),
-                height: 200,
-                color: Colors.blue,
-              ),
-              Container(
-                margin: EdgeInsets.only(bottom: 10),
-                height: 200,
-                color: Colors.orange,
-              ),
-              Container(
-                margin: EdgeInsets.only(bottom: 10),
-                height: 200,
-                color: Colors.red,
-              ),
-              Container(
-                margin: EdgeInsets.only(bottom: 10),
-                height: 200,
-                color: Colors.green,
-              ),
-              Container(
-                margin: EdgeInsets.only(bottom: 10),
-                height: 200,
-                color: Colors.grey,
-              ),
-            ],
-          ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  time = DateTime.now();
+                });
+              },
+              child: const Text('Current Time'),
+            ),
+          ],
         ),
       ),
     );
