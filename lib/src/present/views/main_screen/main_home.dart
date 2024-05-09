0499106ffaa6f48595eebc40bec0e83ac8eac4b9
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-09 21:22:08 +0530

    gridview builder

--- lib/src/present/views/main_screen/main_home.dart
+++ lib/src/present/views/main_screen/main_home.dart
@@ -25,63 +25,20 @@ class _MainHomeScreenState extends State<MainHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: const Text('Flutter Demo'),
-        elevation: 2,
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            const Text(
-              'Select Date',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                DateTime? pickdate = await showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime(2020),
-                  lastDate: DateTime(2025),
-                );
-
-                if (pickdate != null) {
-                  print(pickdate);
-                }
-              },
-              child: const Text('Pick Date'),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            const Text(
-              'Select Time',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                TimeOfDay? picktime = await showTimePicker(
-                  context: context,
-                  initialTime: TimeOfDay.now(),
-                  initialEntryMode: TimePickerEntryMode.dial,
-                );
-
-                if (picktime != null) {
-                  print(picktime);
-                }
-              },
-              child: const Text('Pick Time'),
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text('Flutter Demo'),
+          elevation: 2,
         ),
-      ),
-    );
+        body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 10),
+          itemBuilder: (context, index) {
+            return Container(
+              color: arrColors[index],
+            );
+          },
+          itemCount: arrColors.length,
+        ));
   }
 }
