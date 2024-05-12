8474247d106ed916aeb63e0a8f21ecc105dfbdc8
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-12 22:00:23 +0530

    hero_animation

---
+++ lib/src/present/views/home_screen/hero_animation.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HeroAnimationScreen extends StatelessWidget {
+  const HeroAnimationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Hero Animation'),
+      ),
+      body: Hero(
+        tag: 'background',
+        child: Image.asset(
+          'assets/images/bg_hero.jpeg',
+        ),
+      ),
+    );
+  }
+}
--- lib/src/present/views/main_screen/main_home.dart
+++ lib/src/present/views/main_screen/main_home.dart
@@ -1,3 +1,4 @@
+import 'package:firstapp/src/present/views/home_screen/hero_animation.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -44,95 +45,129 @@ class _MainHomeScreenState extends State<MainHomeScreen> {
       body: SingleChildScrollView(
         child: SizedBox(
           child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                AnimatedContainer(
-                  duration: Duration(seconds: 2),
-                  width: _width,
-                  height: _height,
-                  color: bgColor,
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      if (flag) {
-                        _width = 300.0;
-                        _height = 300.0;
-                        flag = false;
-                        bgColor = Colors.redAccent;
-                      } else {
-                        _width = 200.0;
-                        _height = 200.0;
-                        flag = true;
-                        bgColor = Colors.orange;
-                      }
-                    });
-                  },
-                  child: Text('Animatation'),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                AnimatedOpacity(
-                  opacity: myOpacity,
-                  duration: Duration(seconds: 1),
-                  curve: Curves.fastOutSlowIn,
-                  child: Container(
-                    width: 200,
-                    height: 200,
-                    color: Colors.amber,
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AnimatedContainer(
+                    duration: Duration(seconds: 2),
+                    width: _width,
+                    height: _height,
+                    color: bgColor,
                   ),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      if (opflag) {
-                        myOpacity = 0.0;
-                        opflag = false;
-                      } else {
-                        myOpacity = 1.0;
-                        opflag = true;
-                      }
-                    });
-                  },
-                  child: Text('Opacity'),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                AnimatedCrossFade(
-                  firstChild: Image.asset(
-                    'assets/images/card-close.png',
-                    width: 300,
-                    height: 300,
+                  SizedBox(
+                    height: 5,
                   ),
-                  secondChild: Image.asset(
-                    'assets/images/card-open.png',
-                    width: 300,
-                    height: 300,
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        if (flag) {
+                          _width = 300.0;
+                          _height = 300.0;
+                          flag = false;
+                          bgColor = Colors.redAccent;
+                        } else {
+                          _width = 200.0;
+                          _height = 200.0;
+                          flag = true;
+                          bgColor = Colors.orange;
+                        }
+                      });
+                    },
+                    child: Text('Animatation'),
                   ),
-                  crossFadeState: isFirst
-                      ? CrossFadeState.showFirst
-                      : CrossFadeState.showSecond,
-                  duration: Duration(seconds: 2),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    reload();
-                  },
-                  child: Text('Cross-Fade'),
-                ),
-              ],
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  AnimatedOpacity(
+                    opacity: myOpacity,
+                    duration: Duration(seconds: 1),
+                    curve: Curves.fastOutSlowIn,
+                    child: Container(
+                      width: 200,
+                      height: 200,
+                      color: Colors.amber,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        if (opflag) {
+                          myOpacity = 0.0;
+                          opflag = false;
+                        } else {
+                          myOpacity = 1.0;
+                          opflag = true;
+                        }
+                      });
+                    },
+                    child: const Text('Opacity'),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  AnimatedCrossFade(
+                    firstChild: Image.asset(
+                      'assets/images/card-close.png',
+                      width: 300,
+                      height: 300,
+                    ),
+                    secondChild: Image.asset(
+                      'assets/images/card-open.png',
+                      width: 300,
+                      height: 300,
+                    ),
+                    crossFadeState: isFirst
+                        ? CrossFadeState.showFirst
+                        : CrossFadeState.showSecond,
+                    duration: Duration(seconds: 2),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      reload();
+                    },
+                    child: const Text('Cross-Fade'),
+                  ),
+                  const SizedBox(
+                    height: 60,
+                  ),
+                  const Text(
+                    'Hero Animation',
+                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const HeroAnimationScreen(),
+                        ),
+                      );
+                    },
+                    child: Hero(
+                      tag: 'background',
+                      child: Image.asset(
+                        'assets/images/bg_hero.jpeg',
+                        width: 180,
+                        height: 100,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 100,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
